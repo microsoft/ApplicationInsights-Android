12abcf10f079fa2325a1a8602df0f6b75ad3d4fe
scsouthw <scsouthw@microsoft.com>
2014-12-16 18:04:53 -0800

    Fix JSON serialization in Core library (support java 1.6)

--- core/src/main/java/com/microsoft/applicationinsights/channel/contracts/shared/JsonHelper.java
+++ core/src/main/java/com/microsoft/applicationinsights/channel/contracts/shared/JsonHelper.java
@@ -1,15 +1,66 @@
 package com.microsoft.applicationinsights.channel.contracts.shared;
 
+import com.microsoft.applicationinsights.channel.contracts.DataPoint;
+
 import java.io.IOException;
 import java.io.Writer;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * This is the helper class to have the json to integer/double/string and ext
  * converting.
  */
 public final class JsonHelper {
+    private static final int controlCharacterRange = 0x80;
+    private static final String[] controlCharacters;
+    static {
+        controlCharacters = new String[controlCharacterRange];
+
+        // per RFC 4627 (JSON specification) section 2.5; escape control characters U+0000 -> U+001F
+        for(int i = 0; i <= 0x1f; i++) {
+            controlCharacters[i] = String.format("\\u%04X", i);
+        }
+
+        // additionally escape quotation mark, reverse solidus, line breaks and whitespace
+        controlCharacters['\"'] = "\\\"";
+        controlCharacters['\\'] = "\\\\";
+        controlCharacters['\b'] = "\\b";
+        controlCharacters['\f'] = "\\f";
+        controlCharacters['\n'] = "\\n";
+        controlCharacters['\r'] = "\\r";
+        controlCharacters['\t'] = "\\t";
+    }
+
+    private static String escapeJSON(String input) {
+        StringBuilder builder = new StringBuilder();
+        for(int i = 0; i < input.length(); i++) {
+            char charIndex = input.charAt(i);
+            if(charIndex < controlCharacterRange) {
+                String replacement = controlCharacters[charIndex];
+                if (replacement == null) {
+                    builder.append(charIndex);
+                } else {
+                    builder.append(replacement);
+                }
+            } else if(charIndex== '\u2028') {
+                // JavaScript interprets '\u2028' as newline
+                builder.append("\\u2028");
+            } else if(charIndex == '\u2029') {
+                // JavaScript interprets '\u2029' as newline
+                builder.append("\\u2029");
+            } else {
+                builder.append(charIndex);
+            }
+        }
+
+        return builder.toString();
+    }
+
+
     /**
      * prevent caller to construct this object.
      */
@@ -75,33 +126,16 @@ public final class JsonHelper {
     public static String convert(String value) {
         if (value == null) {
             return "null";
-        }
-
-        if (value.length() == 0) {
+        } else if (value.length() == 0) {
             return "\"\"";
-        }
-
-        // temporary trick to apply JSON encoding to the string
-        JSONObject o = new JSONObject();
-        o.put("v", value);
-        String jsonString = o.toString();
-        int begPos = jsonString.indexOf(':');
-        if (begPos != -1) {
-            begPos = jsonString.indexOf('"', begPos);
-        }
-
-        int endPos = jsonString.lastIndexOf('"');
-        if (begPos != -1 && endPos != -1) {
-            return jsonString.substring(begPos, endPos + 1);
-        }
-        else {
-            return "\"" + value + "\"";
+        } else {
+            return escapeJSON(value);
         }
     }
 
     /**
      * serialize the IJsonSerializable to writer
-     * 
+     *
      * @param writer Writer object
      * @param value IJsonSerializable object
      * @throws IOException
@@ -115,37 +149,91 @@ public final class JsonHelper {
 
     /**
      * serialize the map object to writer
-     * 
+     *
      * @param writer Writer object
-     * @param value Map object
+     * @param map Map object
      * @throws IOException
      */
-    public static <K, V> void writeDictionary(Writer writer, Map<K, V> value) throws IOException {
-        if (value == null) {
+    public static <T> void writeDictionary(Writer writer, Map<String, T> map) throws IOException {
+        if (map == null) {
             writer.write("null");
-        }
+        } else {
+            Set<String> keys = map.keySet();
+            Iterator<String> iterator = keys.iterator();
 
-        JSONObject o = new JSONObject(value);
-        writer.write(o.toString());
+            // special first case to ensure we have contents and that
+            if(iterator.hasNext()) {
+                writer.write("{");
+                String key = iterator.next();
+                T item = map.get(key);
+
+                writer.write(key);
+                writer.write(":");
+                writeItem(writer, item);
+
+                while (iterator.hasNext()) {
+                    key = iterator.next();
+
+                    writer.write(",");
+                    writer.write(key);
+                    writer.write(":");
+
+                    item = map.get(key);
+                    writeItem(writer, item);
+                }
+
+                writer.write("}");
+            }
+        }
     }
 
     /**
      * serialize the List object to writer
-     * 
+     *
      * @param writer Writer object
-     * @param value List object
+     * @param list List object
      * @throws IOException
      */
-    public static <V> void writeList(Writer writer, List<V> value) throws IOException {
-        if (value == null) {
+    public static <T extends IJsonSerializable> void writeList(Writer writer, List<T> list) throws IOException {
+        if (list == null) {
             writer.write("null");
-        }
+        } else {
+            Iterator<T> iterator = list.iterator();
+            if(iterator.hasNext()) {
+                writer.write("[");
 
-        JSONArray jsonArr = new JSONArray();
-        for (V singleValue : value) {
-            jsonArr.put(singleValue);
-        }
+                IJsonSerializable item = iterator.next();
+                item.serialize(writer);
 
-        writer.write(jsonArr.toString());
+                while(iterator.hasNext()){
+                    item = iterator.next();
+                    writer.write(",");
+                    item.serialize(writer);
+                }
+
+                writer.write("]");
+            }
+        }
+    }
+
+    /**
+     * Emits a primitive item of unknown type to the writer
+     * @param writer The writer to which this item will be emitted
+     * @param item The item to write
+     * @param <T> The type of the item
+     * @throws IOException if no handler exists for the type
+     */
+    private static <T> void writeItem(Writer writer, T item) throws IOException {
+        if (item instanceof String) {
+            writer.write(JsonHelper.convert((String) item));
+        } else if (item instanceof Double) {
+            writer.write(JsonHelper.convert((Double) item));
+        } else if (item instanceof Integer) {
+            writer.write(JsonHelper.convert((Integer) item));
+        } else if (item instanceof Long) {
+            writer.write(JsonHelper.convert((Long) item));
+        } else {
+            throw new IOException("Cannot serialize: " + item.toString());
+        }
     }
 }
