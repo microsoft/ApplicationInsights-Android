3d922e02ce5542e45f9e2f2ce17cd1f3cb624b12
Christoph Wendt <chwend@microsoft.com>
2015-04-08 18:26:14 +0200

    Add umbrella header

---
+++ applicationinsights-android/src/main/java/com/microsoft/applicationinsights/AppInsights.java
@@ -0,0 +1,158 @@
+package com.microsoft.applicationinsights;
+
+import android.app.Application;
+import android.content.Context;
+
+import com.microsoft.applicationinsights.internal.Channel;
+import com.microsoft.applicationinsights.internal.EnvelopeFactory;
+import com.microsoft.applicationinsights.internal.TelemetryContext;
+import com.microsoft.applicationinsights.internal.logging.InternalLogging;
+
+import java.util.Map;
+
+public enum AppInsights {
+    INSTANCE;
+
+    private boolean telemetryDisabled;
+    private boolean exceptionTrackingDisabled;
+    private String instrumentationKey;
+    private Context context;
+
+    /**
+     * The configuration for this telemetry client.
+     */
+    protected SessionConfig config;
+
+    /**
+     * Properties associated with this telemetryContext.
+     */
+    private Map<String, String> commonProperties;
+
+    /**
+     * Create AppInsights instance
+     */
+    private AppInsights(){
+        this.telemetryDisabled = false;
+        this.exceptionTrackingDisabled = false;
+    }
+
+    /**
+     * Configure AppInsights
+     * Note: This should be called before start
+     *
+     * @param context the context associated with AppInsights
+     */
+    public static void setup(Context context){
+        AppInsights.INSTANCE.setupInstance(context, null);
+    }
+
+    /**
+     * Configure AppInsights
+     * Note: This should be called before start
+     *
+     * @param context the context associated with AppInsights
+     * @param instrumentationKey the instrumentation key associated with the app
+     */
+    public static void setup(Context context, String instrumentationKey){
+        AppInsights.INSTANCE.setupInstance(context, instrumentationKey);
+    }
+
+    /**
+     * Configure AppInsights
+     * Note: This should be called before start
+     *
+     * @param context the context associated with AppInsights
+     * @param instrumentationKey the instrumentation key associated with the app
+     */
+    public void setupInstance(Context context, String instrumentationKey){
+        this.context = context;
+        this.instrumentationKey = instrumentationKey;
+        this.config = new SessionConfig(this.context);
+    }
+
+    /**
+     * Start AppInsights
+     * Note: This should be called after {@link com.microsoft.applicationinsights.AppInsights#setup(android.content.Context)}
+     */
+    public static void start(){
+        INSTANCE.startInstance();
+    }
+
+    /**
+     * Start AppInsights
+     * Note: This should be called after {@link com.microsoft.applicationinsights.AppInsights#setup(android.content.Context)}
+     */
+    public void startInstance(){
+        String iKey = null;
+        if(this.instrumentationKey != null){
+            iKey = this.instrumentationKey;
+        }else{
+            config.getInstrumentationKey();
+        }
+        TelemetryContext telemetryContext = new TelemetryContext(this.context, iKey);
+        EnvelopeFactory.INSTANCE.configure(telemetryContext);
+
+        if(!this.exceptionTrackingDisabled){
+            ExceptionTracking.registerExceptionHandler(this.context);
+        }
+    }
+
+    /**
+     * Triggers persisting and if applicable sending of queued data
+     * note: this will be called
+     * {@link com.microsoft.applicationinsights.internal.TelemetryQueueConfig#maxBatchIntervalMs} after
+     * tracking any telemetry so it is not necessary to call this in most cases.
+     */
+    public static void sendPendingData() {
+        Channel.INSTANCE.getQueue().flush();
+    }
+
+    /**
+     * Enable auto page view tracking as well as auto session tracking. This will only work, if
+     * {@link com.microsoft.applicationinsights.AppInsights#telemetryDisabled} is set to false.
+     *
+     * @param application the application used to register the life cycle callbacks
+     */
+    public static void enableActivityTracking(Application application){
+        if(!INSTANCE.telemetryDisabled){
+            TelemetryClient.INSTANCE.enableActivityTracking(application);
+        }
+    }
+
+    /**
+     * Enable / disable tracking of unhandled exceptions.
+     *
+     * @param disabled if set to true, crash reporting will be disabled
+     */
+    public static void setExceptionTracking(boolean disabled){
+        INSTANCE.exceptionTrackingDisabled = disabled;
+    }
+
+    /**
+     * Enable / disable tracking of telemetry data.
+     *
+     * @param disabled if set to true, the telemetry feature will be disabled
+     */
+    public static void setTelemetryDisabled(boolean disabled){
+        INSTANCE.telemetryDisabled = disabled;
+    }
+
+    /**
+     * Gets the properties which are common to all telemetry sent from this client.
+     *
+     * @return common properties for this telemetry client
+     */
+    public static Map<String, String> getCommonProperties() {
+        return INSTANCE.commonProperties;
+    }
+
+    /**
+     * Sets properties which are common to all telemetry sent form this client.
+     *
+     * @param commonProperties a dictionary of properties to enqueue with all telemetry.
+     */
+    public static void setCommonProperties(Map<String, String> commonProperties) {
+        INSTANCE.commonProperties = commonProperties;
+        EnvelopeFactory.INSTANCE.setCommonProperties(commonProperties);
+    }
+}
