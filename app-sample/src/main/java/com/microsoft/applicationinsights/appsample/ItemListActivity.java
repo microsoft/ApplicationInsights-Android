3612aaa886f2578388cf1171a434cf5dab487ab8
chrwend <chwend@microsoft.com>
2015-04-23 13:03:58 +0200

    Remove unused import

--- app-sample/src/main/java/com/microsoft/applicationinsights/appsample/ItemListActivity.java
+++ app-sample/src/main/java/com/microsoft/applicationinsights/appsample/ItemListActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 
-import com.microsoft.applicationinsights.contracts.Application;
 import com.microsoft.applicationinsights.library.ApplicationInsights;
 import com.microsoft.applicationinsights.library.config.ApplicationInsightsConfig;
 
