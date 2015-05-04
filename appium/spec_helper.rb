f24e2e303f9b42ad22561300a7e2129a2c633b42
Benny Reimold <benjamin.reimold@codenauts.de>
2015-05-04 20:36:54 +0200

    appium can launch the app

---
+++ appium/spec_helper.rb
@@ -0,0 +1,29 @@
+require 'rubygems'
+require 'rspec'
+require 'rspec/expectations'
+require 'appium_lib'
+require 'byebug'
+
+RSpec.configure do |config|
+  config.color_enabled = true
+  config.before(:all) do
+    options = {
+      caps: {
+        platformName: 'Android',
+       #appActivity: 'ItemListActivity',
+        app:'app-sample/build/outputs/apk/app-sample-debug.apk',
+        #appPackage: 'com.microsoft.applicationinsights.appsample',
+        deviceName: 'appinsights-appium'
+      },
+      launchTimeout: 5000
+    }
+
+    driver = Appium::Driver.new(options).start_driver
+    driver.manage.timeouts.implicit_wait = 10
+    Appium.promote_appium_methods Object
+  end
+
+  config.after(:all) do
+    driver_quit
+  end
+end
