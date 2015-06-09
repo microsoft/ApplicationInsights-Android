db77a89c91ab3d6a2fa8039d5004e88e942c506f
Matthias Wenz <mat@bowstreet.de>
2015-06-09 11:58:13 +0200

    Use Util method to determine feature availability

--- applicationinsights-android/src/main/java/com/microsoft/applicationinsights/library/ApplicationInsights.java
+++ applicationinsights-android/src/main/java/com/microsoft/applicationinsights/library/ApplicationInsights.java
@@ -203,7 +203,7 @@ public enum ApplicationInsights {
             Application application = INSTANCE.getApplication();
 
             if (INSTANCE.getApplication() != null &&
-                    Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH &&
+                    Util.isLifecycleTrackingAvailable() &&
                     !this.autoCollectionDisabled) {
                 LifeCycleTracking.initialize(telemetryContext, this.config);
                 LifeCycleTracking.registerForPersistingWhenInBackground(application);
