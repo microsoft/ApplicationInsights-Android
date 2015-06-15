3c1d643203b2a3ee79e21fb3f8965a6979051856
Benjamin Reimold <bereimol@microsoft.com>
2015-06-15 14:43:58 +0200

    Added backgrounding to appium script

--- appium/integration/prototype_spec.rb
+++ appium/integration/prototype_spec.rb
@@ -6,7 +6,7 @@ describe 'When we open our sample app' do
       #scroll_to('Track event')
     #end
 
-  it 'should be able to tap on  Track event' do
+  it 'should should tap track 5 times' do
     list_el = text('Track event')
     list_el.click
     back
@@ -16,7 +16,17 @@ describe 'When we open our sample app' do
     back
     list_el.click
     back
+    list_el.click
+    back
   end
+
+it 'should background and foreground the app 3 times' do 
+  background_app(3)
+  sleep(2)
+  background_app(3)
+  sleep(2)
+  background_app(3)
+end
 
   it 'should trigger a sync' do
     list_el = text('Trigger Synchronize')
