6b50a9a652b140728b8979bebeb64c16de930c52
Benjamin Reimold <bereimol@microsoft.com>
2015-06-15 15:59:54 +0200

    Extend appium script with backgrounding

--- appium/integration/prototype_spec.rb
+++ appium/integration/prototype_spec.rb
@@ -1,10 +1,6 @@
 require File.expand_path('spec_helper')
 
-describe 'When we open our sample app' do
-  #describe 'Tap track event' do
-    #before do
-      #scroll_to('Track event')
-    #end
+def defaultTestRun
 
   it 'should should tap track 5 times' do
     list_el = text('Track event')
@@ -20,12 +16,31 @@ describe 'When we open our sample app' do
     back
   end
 
-it 'should background and foreground the app 3 times' do 
+it 'should background for 3s and foreground again 3 times' do 
   background_app(3)
   sleep(2)
   background_app(3)
   sleep(2)
   background_app(3)
+end
+
+it 'should should tap track 5 times' do
+    list_el = text('Track event')
+    list_el.click
+    back
+    list_el.click
+    back
+    list_el.click
+    back
+    list_el.click
+    back
+    list_el.click
+    back
+  end
+
+  it 'should background for 10s and foreground again' do 
+  background_app(10)
+  sleep(2)
 end
 
   it 'should trigger a sync' do
@@ -37,10 +52,65 @@ end
     back
   end
 
-
 it 'should crash the app' do
   list_el = text('Crash the App!')
   list_el.click
 end
-
+  
 end
+
+describe 'Run default tests' do
+  defaultTestRun
+end
+
+
+describe 'Run with disabled session management' do
+  it 'Can disable session management' do
+ list_el = text('Disable session management')
+  list_el.click
+  back
+end
+  defaultTestRun
+end
+
+describe 'Run with re-enabled session management' do
+  it 'Can enable session management' do
+ list_el = text('Enable session management')
+  list_el.click
+  back
+end
+  defaultTestRun
+end
+
+describe 'Run with disabled pageviews' do
+  it 'Can disable pageviews' do
+ list_el = text('Disable page view tracking')
+  list_el.click
+  back
+end
+  defaultTestRun
+end
+
+describe 'Run with re-enabled pageviews' do
+  it 'Can re-enable pageviews' do
+ list_el = text('Enable page view tracking')
+  list_el.click
+  back
+end
+  defaultTestRun
+end
+
+describe 'Run with disabled pageviews and session management' do
+  it 'Can disable pageviews' do
+ list_el = text('Disable page view tracking')
+  list_el.click
+  back
+end
+ it 'Can disable session management' do
+ list_el = text('Disable session management')
+  list_el.click
+  back
+end
+  defaultTestRun
+end
+
