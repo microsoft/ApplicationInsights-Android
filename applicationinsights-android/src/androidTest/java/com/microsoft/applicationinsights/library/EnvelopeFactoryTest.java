1e61565d50e81d8d69427af32bf0755090831904
Christoph Wendt <chwend@microsoft.com>
2015-07-01 11:10:31 -0700

    Add test: Parse java exception frame

--- applicationinsights-android/src/androidTest/java/com/microsoft/applicationinsights/library/EnvelopeFactoryTest.java
+++ applicationinsights-android/src/androidTest/java/com/microsoft/applicationinsights/library/EnvelopeFactoryTest.java
@@ -154,7 +154,7 @@ public class EnvelopeFactoryTest extends InstrumentationTestCase {
     }
 
 
-    public void testParseSingleManagedStackframeFromStringWorks(){
+    public void testParseSingleManagedStackframeFromStringWorks() {
         // Setup
         String testString1 = "  at My.Method/Name (My.Parameter Type) in My/Filename:123 ";
         // Test
@@ -195,6 +195,35 @@ public class EnvelopeFactoryTest extends InstrumentationTestCase {
         StackFrame frame5 = sut.getStackframe(testString5, true);
         // Verify
         Assert.assertNull(frame5);
+    }
+
+    public void testParseSingleUnmanagedStackframeFromStringWorks() {
+        // Setup
+        String testString1 = "  at com.microsoft.ai.xamarinexample.ExampleClass$1.run(ExampleClass.java:17)  ";
+        // Test
+        StackFrame frame1 = sut.getStackframe(testString1, false);
+        // Verify
+        Assert.assertEquals("com.microsoft.ai.xamarinexample.ExampleClass$1.run", frame1.getMethod());
+        Assert.assertEquals("ExampleClass.java", frame1.getFileName());
+        Assert.assertEquals(17, frame1.getLine());
+
+        // Setup
+        String testString2 = "at dalvik.system.NativeStart.main(Native Method)";
+        // Test
+        StackFrame frame2 = sut.getStackframe(testString2, false);
+        // Verify
+        Assert.assertEquals("dalvik.system.NativeStart.main", frame2.getMethod());
+        Assert.assertNull(frame2.getFileName());
+        Assert.assertEquals(0, frame2.getLine());
+
+        // Setup
+        String testString3 = "at () md5d4dd78677dce656d5db26c85a3743ef3.TableViewModelRenderer.onItemClick(TableViewModelRenderer.java:99)";
+        // Test
+        StackFrame frame3 = sut.getStackframe(testString3, false);
+        // Verify
+        Assert.assertEquals("() md5d4dd78677dce656d5db26c85a3743ef3.TableViewModelRenderer.onItemClick", frame3.getMethod());
+        Assert.assertEquals("TableViewModelRenderer.java", frame3.getFileName());
+        Assert.assertEquals(99, frame3.getLine());
     }
 
     // TestHelper
